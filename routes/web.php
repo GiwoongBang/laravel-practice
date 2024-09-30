4c2befc443bd4b1da94b2245c57254d7a50f4da2
Giwoong Bang <bku6713@benefitplus.kr>
2024-09-30 10:29:14 +0900

    feat: Add Post feature(getAllPosts)

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use App\Http\Controllers\ArrayController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SessionController;
 use App\Http\Controllers\StringController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,22 +28,22 @@ Route::get('/home/{name?}', [HomeController::class, 'index'])->name('home.index'
 
 ////////////////////////////////////
 
-/*
- * Spring의 @PathVariable (경로변수)
- */
-Route::get('/users/{name?}', function ($name = null) {
-    return view('users', compact('name'));
-});
-
-/*
- * Spring의 @RequestParam (쿼리스트링)
- */
-Route::get('/users', function (Request $request) {
-    $name = $request->input('name', null);
-    $age = $request->input('age', null);
-
-    return view('users', compact('name', 'age'));
-});
+///*
+// * Spring의 @PathVariable (경로변수)
+// */
+//Route::get('/users/{name?}', function ($name = null) {
+//    return view('users', compact('name'));
+//});
+//
+///*
+// * Spring의 @RequestParam (쿼리스트링)
+// */
+//Route::get('/users', function (Request $request) {
+//    $name = $request->input('name', null);
+//    $age = $request->input('age', null);
+//
+//    return view('users', compact('name', 'age'));
+//});
 
 ////////////////////////////////////
 
@@ -49,18 +53,40 @@ Route::get('/grammar', function () {
 
 ////////////////////////////////////
 
-Route::get('/posts', [ClientController::class, 'getAllPost']) -> name('post.getall');
-
-Route::get('/posts/{id}', [ClientController::class, 'getPostById']) -> name('post.getbyid');
-
-Route::get('/add-post', [ClientController::class, 'addPost']) -> name('post.add');
-
-Route::get('/update-post', [ClientController::class, 'updatePost']) -> name('post.update');
-
-Route::get('/delete-post/{id}', [ClientController::class, 'deletePost']) -> name('post.delete');
+//Route::get('/posts', [ClientController::class, 'getAllPost']) -> name('post.getall');
+//
+//Route::get('/posts/{id}', [ClientController::class, 'getPostById']) -> name('post.getbyid');
+//
+//Route::get('/add-post', [ClientController::class, 'addPost']) -> name('post.add');
+//
+//Route::get('/update-post', [ClientController::class, 'updatePost']) -> name('post.update');
+//
+//Route::get('/delete-post/{id}', [ClientController::class, 'deletePost']) -> name('post.delete');
 
 ////////////////////////////////////
 
 Route::get('/string', [StringController::class, 'index']) -> name('string.index');
 
 Route::get('/array', [ArrayController::class, 'index']) -> name('array.index');
+
+////////////////////////////////////
+
+Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::post('/login', [LoginController::class, 'loginSubmit']) -> name('login.submit');
+
+////////////////////////////////////
+
+Route::get('/session/get', [SessionController::class, 'getSession']) -> name('session.get');
+
+Route::get('/session/store', [SessionController::class, 'storeSession']) -> name('session.store');
+
+Route::get('/session/delete', [SessionController::class, 'deleteSession']) -> name('session.delete');
+
+////////////////////////////////////
+
+Route::get('/posts', [PostController::class, 'getAllPosts']) -> name('posts.getallposts');
+
+
